a36eccafa9a394033b933ffd186034be7dbea9de
sirozu <rychen123@yandex.ru>
2022-08-02 00:48:24 +0300

    added animations and jump

--- Source/InitialShootThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/InitialShootThemUp/Private/Player/STUBaseCharacter.cpp
@@ -1,6 +1,5 @@
 // Shoot Them Up Game. All Rights Reserved
 
-
 #include "Player/STUBaseCharacter.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
@@ -8,10 +7,11 @@
 // Sets default values
 ASTUBaseCharacter::ASTUBaseCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need
+    // it.
+    PrimaryActorTick.bCanEverTick = true;
 
-	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
     SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
 
     SpringArmComponent->SetupAttachment(GetRootComponent());
@@ -25,33 +25,33 @@ ASTUBaseCharacter::ASTUBaseCharacter()
 // Called when the game starts or when spawned
 void ASTUBaseCharacter::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
 }
 
 // Called every frame
 void ASTUBaseCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+    Super::Tick(DeltaTime);
 }
 
 // Called to bind functionality to input
-void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	PlayerInputComponent->BindAxis("MoveForward", this, &ASTUBaseCharacter::MoveForward);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+    PlayerInputComponent->BindAxis("MoveForward", this, &ASTUBaseCharacter::MoveForward);
     PlayerInputComponent->BindAxis("MoveRight", this, &ASTUBaseCharacter::MoveRight);
 
-	PlayerInputComponent->BindAxis("LookHorizontal", this, &ASTUBaseCharacter::AddControllerYawInput);
+    PlayerInputComponent->BindAxis("LookHorizontal", this, &ASTUBaseCharacter::AddControllerYawInput);
     PlayerInputComponent->BindAxis("LookVertical", this, &ASTUBaseCharacter::AddControllerPitchInput);
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASTUBaseCharacter::Jump);
 }
 
 void ASTUBaseCharacter::MoveForward(float AxisValue)
 {
-    AddMovementInput(GetActorForwardVector(), AxisValue * MovementSpeed * GetWorld() -> DeltaTimeSeconds);
+    AddMovementInput(GetActorForwardVector(), AxisValue * MovementSpeed * GetWorld()->DeltaTimeSeconds);
 }
 
 void ASTUBaseCharacter::MoveRight(float AxisValue)
 {
-    AddMovementInput(GetActorRightVector(), AxisValue * MovementSpeed * GetWorld() -> DeltaTimeSeconds);
+    AddMovementInput(GetActorRightVector(), AxisValue * MovementSpeed * GetWorld()->DeltaTimeSeconds);
 }
--- Source/InitialShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/InitialShootThemUp/Public/Player/STUBaseCharacter.h
@@ -12,33 +12,32 @@ class UCameraComponent;
 UCLASS()
 class INITIALSHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
-public:
-	// Sets default values for this character's properties
-	ASTUBaseCharacter();
-	
+  public:
+    // Sets default values for this character's properties
+    ASTUBaseCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+  protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
-	UCameraComponent* CameraComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    UCameraComponent *CameraComponent;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     USpringArmComponent *SpringArmComponent;
 
     void MoveForward(float AxisValue);
     void MoveRight(float AxisValue);
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+  public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-	float MovementSpeed = 100.0f;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+    float MovementSpeed = 100.0f;
 };
